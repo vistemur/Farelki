d40629e4a9cb3da8e9475fcc53ffa1bc0a9f0173
Roman <iboutlet@icloud.com>
2019-04-06 23:08:57 +0300

    Xcode project is made and organized

---
+++ Farelki/Farelki/Supporting files/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Farelki
+//
+//  Created by роман поздняков on 06/04/2019.
+//  Copyright © 2019 romchick. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let startVC = MenuViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = startVC
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+}
+
---
+++ Farelki/Farelki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Farelki
+//
+//  Created by роман поздняков on 06/04/2019.
+//  Copyright © 2019 romchick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
